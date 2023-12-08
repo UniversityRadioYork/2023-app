4e0d5dab011b86cca4b7bea2acff5563058121ea
Big Jimmy <bigjimmy@BigJimmy.Big.Jimmy>
2023-12-08 23:34:03 +0000

    backup

--- app/screens/home/Home.js
+++ app/screens/home/Home.js
@@ -2,17 +2,12 @@ import * as React from 'react';
 import {Button, View, Text, StyleSheet} from 'react-native';
 
 import {sizes, colours} from '../../globals/constants/style';
+import CurrentWebcam from './components/CurrentWebcam';
 
 export default function HomeScreen({navigation}) {
 	return (
-		<View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
-			<Text style={styles.text}>
-				Welcome to the ury app, an app where you can really just fuck about
-			</Text>
-			<Button
-				title="Go to Schedule"
-				onPress={() => navigation.navigate('Schedule')}
-			/>
+		<View style={{flex: 1, alignItems: 'left'}}>
+			<CurrentWebcam />
 		</View>
 	);
 }
